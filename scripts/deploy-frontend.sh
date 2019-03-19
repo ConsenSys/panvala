22744e70ccbc2cc79f9b7b5efd351a51408802a8
Akua Nti <akua.nti@consensys.net>
2019-03-19 17:20:44 -0400

    Update CI deploy to use new scripts
    * Add deploy-api and deploy-frontend scripts
    * Update CI config
    * Cleanup

---
+++ scripts/deploy-frontend.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# deploy frontend
+# deploy-frontend.sh ENVIRONMENT
+# Environment is staging or production
+# Creates service panvala-api-${ENVIRONMENT}
+set -u
+set -e
+
+. scripts/helpers.sh
+
+
+# Deploy
+deploy() {
+    ENVIRONMENT=$1
+    # TAG=$(get_image_tag panvala/api)
+    REPO="$(get_image_registry)/panvala/frontend"
+    APP="panvala-frontend"
+
+    echo helm upgrade --install \
+        --namespace ${ENVIRONMENT} \
+        --set environment=${ENVIRONMENT} \
+        --set image.tag=${TAG} \
+        --set image.repository=${REPO} \
+        --set nameOverride="${APP}" \
+        --set fullnameOverride="${APP}" \
+        "${APP}-${ENVIRONMENT}" \
+        ./charts/${APP}
+}
+
+deploy $1
