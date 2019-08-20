19873309c7da5677b38c963b4332cff01abd77e4
Isaac Kang <isaac.kang@consensys.net>
2019-08-20 23:36:28 +0200

    exit if getWinningSlate reverts

--- api/scripts/setValue.js
+++ api/scripts/setValue.js
@@ -7,32 +7,37 @@ const {
 const { getContracts } = require('../utils/eth');
 const { Request } = require('../models');
 
+const mnemonic = process.env.MNEMONIC;
+
 run();
 
 async function run() {
-  const { signer, gatekeeper } = getContracts();
+  const { provider, gatekeeper } = getContracts();
+  const mnemonicWallet = ethers.Wallet.fromMnemonic(mnemonic);
+  const signer = new ethers.Wallet(mnemonicWallet.privateKey, provider);
+
+  // Settings
   const psAddress = await gatekeeper.functions.parameters();
   const parameters = new ethers.Contract(psAddress, ParameterStore.abi, signer);
-
-  // current epoch
   const epochNumber = await gatekeeper.functions.currentEpochNumber();
 
+  // Winning slate of previous epoch
   let winningSlate;
   try {
-  // get the winning slate of the previous epoch
     winningSlate = await gatekeeper.functions.getWinningSlate(epochNumber.sub(1), psAddress);
   } catch (error) {
-    console.error(error);
+    console.error('Contest not finalized yet. Exiting.');
+    process.exit(0);
   }
 
-  // get the winning slate requests
+  // Winning slate requests
   const slateRequests = await gatekeeper.functions.slateRequests(winningSlate);
 
-  try {
-    // loop the winning slate requests, call setValue if it's not executed
-    await Promise.all(
-      slateRequests.map(async requestID => {
-        // get the Request
+  // call setValue if slate request has not been executed
+  await Promise.all(
+    slateRequests.map(async requestID => {
+      try {
+        // From db
         const request = await Request.findOne({
           where: {
             requestID: requestID.toString(),
@@ -40,22 +45,24 @@ async function run() {
           raw: true,
         });
 
+        // Find matching on-chain proposal
         const proposal = await parameters.functions.proposals(request.proposalID);
 
-        // check if already executed
+        // Ignore if executed
         if (proposal.executed) {
           console.log('proposal already executed');
         } else {
           console.log('setting value for proposal', request.proposalID);
           await parameters.functions.setValue(request.proposalID);
         }
-      })
-    );
+      } catch (error) {
+        console.error(error);
+        process.exit(1);
+      }
+    })
+  );
 
-    console.log('DONE');
-    process.exit(0);
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
+  // Success
+  console.log('DONE');
+  process.exit(0);
 }
