34b47a228df8cafb94bb6bed2a8ada928a21fd75
Isaac Kang <isaac.kang@consensys.net>
2019-09-19 18:14:05 -0400

    fix twitter link

--- website/src/WebsiteModal.js
+++ website/src/WebsiteModal.js
@@ -185,7 +185,7 @@ const StepTwo = ({ message }) => (
 const Tweet = () => {
   return (
     <a
-      class="twitter-share-button"
+      class="link twitter-share-button"
       href="https://twitter.com/intent/tweet?text=I%20just%20made%20a%20donation%20to%20Panvala%20to%20support%20the%20Ethereum%20open%20source%20ecosystem.%20Please%20join%20me%20by%20contributing%20at%20panvala.com&hashtags=panvala,ethereum"
       data-size="large"
     ></a>
@@ -207,7 +207,14 @@ const StepThree = ({ message, handleClose }) => {
         Thank you for donating to Panvala. Each and every Panvala patron plays a key role in moving
         Ethereum forward. You can share your support on Twitter!
       </div>
-      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-around', width: '80%' }}>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-around',
+          width: '80%',
+        }}
+      >
         <div style={styles.cancel} onClick={handleClose}>
           Close
         </div>
