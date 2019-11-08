b1ddb3c92c6b445416eb65c56553e07bce772681
Isaac Kang <isaac.kang@consensys.net>
2019-11-08 07:58:24 -0500

    rm extraneous files for stories

--- gatsby-site/stories/data.stories.js
+++ gatsby-site/stories/data.stories.js
@@ -1,4 +1,5 @@
-import './css/tachyons.css';
-import './css/open-sans.css';
-import './css/colors.css';
-import './css/util.css';
+import '../src/css/clip-path.css';
+import '../src/css/colors.css';
+import '../src/css/open-sans.css';
+import '../src/css/tachyons.css';
+import '../src/css/util.css';
