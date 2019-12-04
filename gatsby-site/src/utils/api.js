311fb3394f5b8b247775a7c12cacf837d6a21dc3
Isaac Kang <isaac.kang@consensys.net>
2019-12-04 14:56:43 -0500

    fix base endpoint

--- gatsby-site/src/utils/api.js
+++ gatsby-site/src/utils/api.js
@@ -1,10 +1,13 @@
+import { getEndpointAndHeaders } from './donate';
+
 export async function getEpochDates() {
-  const endpoint = `http://localhost:5001/api/epochs/current/dates`;
-  const result = await getData(endpoint);
+  const { endpoint, headers } = getEndpointAndHeaders();
+  const route = `${endpoint}/api/epochs/current/dates`;
+  const result = await getData(route, headers);
   console.log('result:', result);
   return result;
 }
 
-export async function getData(endpoint) {
-  return fetch(endpoint).then(res => res.json());
+export async function getData(endpoint, headers) {
+  return fetch(endpoint, { headers }).then(res => res.json());
 }
